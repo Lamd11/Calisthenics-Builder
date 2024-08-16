75013ed498fe6395eb07e91128bd9f225a267460
Daniel <daniel.lam3987@gmail.com>
2024-08-16 00:04:25 -0400

    Edited space between popup styles.

--- src/components/progressions-components/skill-popup.js
+++ src/components/progressions-components/skill-popup.js
@@ -11,7 +11,7 @@ const SkillPopup = ({ skill, onClose }) => {
             className='fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50'
             onClick={onClose}>
             <div
-                className='bg-white p-4 rounded-md relative overflow-y-auto w-[80%] max-h-[90%]'
+                className='bg-white p-8 rounded-md relative overflow-y-auto w-[80%] max-h-[90%]'
                 onClick={(e) => e.stopPropagation()}>
                 <div
                     className='absolute right-8 top-1 w-[1.5rem] h-[1.5rem] opacity-40 hover:opacity-100'
@@ -22,21 +22,22 @@ const SkillPopup = ({ skill, onClose }) => {
                 <div className='items-center text-center flex flex-col bg-white'>
                     <h2 className='text-6xl font-bold text-gray-800 mb-4'>{skill.name}</h2>
                     <img src={skill.image} alt={skill.name} className='object-cover w-[60%] max-h-[40%] rounded-sm mb-6' />
-                    <div className='flex justify-between'>
-                        <div className='w-1/4'>
+                    <div className='flex flex-col md:flex-row w-full justify-center gap-8'>
+                        <div className='w-full md:w-1/4 jusitfy-center'>
                             <MuscleTable muscleList={skill.muscles} />
                         </div>
-                        <div className='w-3/4'>
-                            <p className='ml-12 mr-12'>{skill.description}</p>
+                        <div className='w-full md:w-3/4'>
+                            <h3 className='text-2xl font-semibold text-gray-800 text-left'>Info</h3>
+                            <p className='text-gray-700 mt-4 text-left'>{skill.description}</p>
                         </div>
                     </div>
                     <div id="difficulty-bar"></div>
                     <h3 className="text-2xl mb-4 font-bold">Progression Steps</h3>
                     <div className='flex flex-col md:flex-row justify-between gap-4'>
-                        <div className='w-1/4'>
+                        <div className='w-full md:w-1/4'>
                             <DifficultyLegend />
                         </div>
-                        <div className='w-3/4'>
+                        <div className='w-full md:w-3/4'>
                             <SkillProgression progressions={skill.progressions} />
                         </div>
                     </div>
